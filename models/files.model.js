19b5329d21c77a2b49479dac1306d74a39938042
Harsh Prajapati <harshprajapaticoding@gmail.com>
2025-08-15 10:34:16 +0530

    updated the file model

--- models/files.model.js
+++ models/files.model.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
 const fileSchema = mongoose.Schema({
-    fileName:{
-        type:String,
-        required:[true, "fileName is required."],
-    },
-    storageId:{
-        type:String,
-    },
-    userId:{
-        type:mongoose.Types.ObjectId,
-        ref:"users"
-    },
-    
-})
+  fileName: {
+    type: String,
+    required: [true, "fileName is required."],
+  },
+  storageId: {
+    type: String,
+  },
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "users",
+  },
+});
 
 const fileModel = mongoose.model("file", fileSchema)
 
