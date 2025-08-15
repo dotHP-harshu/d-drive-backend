03d5884ae5092ad23ae317c5b4006ebc12ae4b16
Harsh Prajapati <harshprajapaticoding@gmail.com>
2025-08-15 10:33:22 +0530

    git updated the response structure

--- config/supabase-client.js
+++ config/supabase-client.js
@@ -1,9 +1,11 @@
 const {createClient}  = require("@supabase/supabase-js")
+require("dotenv").config();
+
+const url = process.env.SUPABASE_URL;
+
+const key = process.env.SUPABASE_KEY;
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
-  "https://yjijawbttohrgrmcjjpd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqaWphd2J0dG9ocmdybWNqanBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjE3ODMsImV4cCI6MjA3MDEzNzc4M30.RxpLgZqaEgGf_Ng7MhAUoxjfG1WmpPJ2CusGszz31T4"
-);
+const supabase = createClient(url, key);
 
 module.exports = supabase
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,19 +4,22 @@ function auth(req, res, next) {
   const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).json({ message: "Unauthorised activity" });
+    return res
+      .status(401)
+      .json({ success: false, code: 401, message: "Unauthorised activity" });
   }
 
   try {
     const verification = jwt.verify(token, process.env.SECRET_KEY);
 
-    req.user = verification
+    req.user = verification;
 
     next();
-
   } catch (err) {
-    res.status(401).json({ message: "Unauthorised activity" });
-    console.log(err)
+    res
+      .status(401)
+      .json({ success: true, code: 401, message: "Unauthorised activity" });
+    console.log(err);
   }
 }
 
