fe563133e2036a1f54b1ab06c1ad10f8911f2ea2
Harsh Prajapati <harshprajapaticoding@gmail.com>
2025-08-15 10:34:44 +0530

    update the routes and the response structure

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -19,10 +19,14 @@ router.post(
     try {
       const { data, error } = await supabase.storage
         .from("d-drive")
-        .upload(`${userId}/${file.originalname}`, file.buffer, { contentType: file.mimetype });
+        .upload(`${userId}/${file.originalname}`, file.buffer, {
+          contentType: file.mimetype,
+        });
 
       if (error) {
-        return res.status(400).json({ message: error.message });
+        return res
+          .status(400)
+          .json({ success: false, code: error.status, message: error.message });
       }
       const newFile = await fileModel.create({
         fileName: file.originalname,
@@ -35,9 +39,15 @@ router.post(
       user.files = [...savedFiles];
       await user.save();
 
-      res.status(200).json({ file: newFile, message: "uploaded succesfully" });
+      res.status(200).json({
+        success: true,
+        code: 200,
+        data: { message: "uploaded succesfully", file: newFile },
+      });
     } catch (err) {
-      return res.status(400).json({ message: "file already exists" });
+      return res
+        .status(400)
+        .json({ message: err.message, success: false, code: 400 });
     }
   }
 );
@@ -48,44 +58,72 @@ router.delete("/delete/:id", authMiddleware, async (req, res) => {
   const { userId } = req.user;
 
   try {
-    const file = await fileModel.findById(id);
-    if (!file) return res.status(400).json({ message: "file not found." });
+    const file = await fileModel.findOne({ storageId: id, userId });
+    if (!file)
+      return res
+        .status(400)
+        .json({ message: "file not found.", success: false, code: 400 });
     const { error } = await supabase.storage
       .from("d-drive")
       .remove([`${userId}/${file.fileName}`]);
     if (error) {
-      return res.status(400).json({ message: error.message });
+      return res
+        .status(400)
+        .json({ message: error.message, code: error.status, success: false });
     }
-    await fileModel.findByIdAndDelete(id);
-    res.status(200).send({ file, message: "Successfully deleted file" });
+    await fileModel.findByIdAndDelete(file._id);
+    res.status(200).send({
+      success: true,
+      code: 200,
+      data: { file, message: "Successfully deleted file" },
+    });
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    return res
+      .status(400)
+      .json({ message: error.message, code: 400, success: false });
   }
 });
 
 // download file
 router.get("/download/:id", authMiddleware, async (req, res) => {
+  const { userId } = req.user;
   const { id } = req.params;
-  const file = await fileModel.findById(id);
-  if (!file) return res.status(400).json({ message: "File not found" });
+  const file = await fileModel.findOne({ storageId: id, userId });
+  if (!file)
+    return res
+      .status(404)
+      .json({ message: "File not found", success: false, code: 404 });
   const { data, error } = await supabase.storage
     .from("d-drive")
-    .createSignedUrl(file.fileName, 60, { download: true });
+    .createSignedUrl(userId + "/" + file.fileName, 60, { download: true });
 
-  if (error) return res.status(400).json({ message: error.message });
+  if (error)
+    return res
+      .status(400)
+      .json({ message: error.message, success: false, code: error.status });
 
-  res.status(200).json({ data, message: "This is download link" });
+  res.status(200).json({
+    success: true,
+    code: 200,
+    data: { signedUrl: data.signedUrl, message: "This is download link" },
+  });
 });
 
 // get all files
 router.get("/all", authMiddleware, async (req, res) => {
   const { userId } = req.user;
 
-  const {data , error} = await supabase.storage.from("d-drive").list(userId, {sortBy:{column:"name", order:"asc"}})
+  const { data, error } = await supabase.storage
+    .from("d-drive")
+    .list(userId, { sortBy: { column: "name", order: "asc" } });
 
-  if(error) return res.status(400).json({ message: error.message });
+  if (error) return res.status(400).json({ message: error.message });
 
-  res.status(200).json({ data, message: "succesfully" });
+  res.status(200).json({
+    success: true,
+    code: 200,
+    data: { files: data, message: "successfully get files" },
+  });
 });
 
 // rename file
@@ -94,27 +132,47 @@ router.post("/rename/:id", authMiddleware, async (req, res) => {
   const { id } = req.params;
   const { userId } = req.user;
 
-  const file = await fileModel.findById(id);
+  const file = await fileModel.findOne({ storageId: id, userId });
+  if (!file)
+    return res
+      .status(400)
+      .json({ message: "file not found.", success: false, code: 400 });
 
   const splitted = file.fileName.split(".");
+
+  if (splitted[0] === newName)
+    return res.status(200).json({
+      data: { file, message: "renamed successfully" },
+      success: true,
+      code: 200,
+    });
+
   splitted[0] = newName;
 
-  const oldName = file.fileName
-  const newFileName = splitted.join(".")
+  const oldName = file.fileName;
+  const newFileName = splitted.join(".");
 
-  const {  error } = await supabase.storage.from("d-drive").move(`${userId}/${oldName}`, `${userId}/${newFileName}`);
+  const { error } = await supabase.storage
+    .from("d-drive")
+    .move(`${userId}/${oldName}`, `${userId}/${newFileName}`);
 
   if (error) {
-    return res.status(400).json({ message: error.message });
+    return res
+      .status(400)
+      .json({ message: error.message, code: 400, success: false });
   }
 
   file.fileName = newFileName; // update the filename in mongoose
 
-  // update the path 
+  // update the path
 
   await file.save();
 
-  res.status(200).json({ message: "renamed successfully" });
+  res.status(200).json({
+    data: { file, message: "renamed successfully" },
+    success: true,
+    code: 200,
+  });
 });
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,27 +15,46 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors, message: "Invalid input" });
+      return res.status(400).json({
+        success: false,
+        code: 400,
+        message: "Invalid input",
+        errors,
+      });
     }
     const { email, username, password } = req.body;
 
-    bcrypt.hash(password, 10, async (err, hash) => {
-      try {
-        const newUser = await userModel.create({
-          email,
-          username,
-          password: hash,
-        });
-        res.status(400).json("User created successfully..");
-      } catch (err) {
-        res
-          .status(400)
-          .json({
-            error: err.errorResponse,
-            message: "User is already registered.",
-          });
-      }
-    });
+    const hash = await bcrypt.hash(password, 10);
+    try {
+      const newUser = await userModel.create({
+        email,
+        username,
+        password: hash,
+      });
+      const token = jwt.sign(
+        { email: newUser.email, userId: newUser._id },
+        process.env.SECRET_KEY
+      );
+      res.status(201).json({
+        success: true,
+        code: 201,
+        data: {
+          token,
+          user: {
+            email: newUser.email,
+            username: newUser.username,
+            userId: newUser._id,
+          },
+        },
+        message: "User created successfully..",
+      });
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+        code: 400,
+        message: "User is already registered.",
+      });
+    }
   }
 );
 
@@ -46,7 +65,9 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ error: errors, message: "Invalid input" });
+      return res
+        .status(400)
+        .json({ message: "Invalid input", success: false, code: 400 });
     }
 
     const { email, password } = req.body;
@@ -54,37 +75,62 @@ router.post(
     const user = await userModel.findOne({ email });
 
     if (!user) {
-      return res
-        .status(400)
-        .json({ message: "User or password is not correct." });
-    }
-
-    const isMatch = bcrypt.compare(password, user.password);
-
-    if(!isMatch){
-       return res
-         .status(400)
-         .json({ message: "User or password is not correct." });
-    }
-    
-    const token = jwt.sign({ email: user.email, userId:user._id }, process.env.SECRET_KEY);
-
-    res.cookie("token", token)
-
-    res
-      .status(200)
-      .json({
-        token,
-        user: { email: user.email, username: user.username },
-        message: "Authentication completed",
+      return res.status(400).json({
+        message: "User or password is not correct.",
+        success: false,
+        code: 400,
       });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "User or password is not correct.",
+        success: false,
+        code: 400,
+      });
+    }
+
+    const token = jwt.sign(
+      { email: user.email, userId: user._id },
+      process.env.SECRET_KEY
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+      code: 200,
+      success: true,
+      message: "Logged in successfully",
+      data: {
+        token,
+        user: { email: user.email, username: user.username, userId: user._id },
+      },
+    });
   }
 );
 
-router.get("/logout", authMiddleware, async (req, res) =>{
-  res.clearCookie("token")
-  res.status(200).json({ message: "Logged out successfully" })
+router.get("/me", authMiddleware, async (req, res) => {
+  const { userId } = req.user;
 
-})
+  const user = await userModel.findById(userId);
+
+  if (!user)
+    return res
+      .status(404)
+      .json({ success: false, message: "User not Found", code: 404 });
+
+  res
+    .status(200)
+    .json({ data: { user, message: "User found" }, success: true, code: 200 });
+});
+
+router.get("/logout", authMiddleware, async (req, res) => {
+  res.clearCookie("token");
+  res
+    .status(200)
+    .json({ message: "Logged out successfully", success: true, code: 200 });
+});
 
 module.exports = router;
